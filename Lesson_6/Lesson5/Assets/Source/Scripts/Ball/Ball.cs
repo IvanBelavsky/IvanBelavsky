6753a9eefca875362981f1681c9f2076a5d74045
Ivan_Belavsky <ivan.belavskiy@gmail.com>
2023-11-02 20:38:16 +0300

    Add HomeWork Lessons 5-6

---
+++ Lesson_6/Lesson5/Assets/Source/Scripts/Ball/Ball.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbode;
+    private void Awake()
+    {
+        _rigidbode = GetComponent<Rigidbody>();
+    }
+    public void Fly(Vector3 directoin, float force)
+    {
+        _rigidbode.AddForce(force * directoin, ForceMode.Impulse);
+    }
+}
